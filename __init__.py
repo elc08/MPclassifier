5ecf3265720395ae0bcf45dac7d80743284c3e42
elc08 <lactovit@hotmail.com>
2021-01-15 08:58:31 +0000

    style clean

--- __init__.py
+++ __init__.py
@@ -10,54 +10,58 @@ from MSclassifier import load, signature, model
 import pickle
 
 
-class signature_model :
-    def __init__ (self):
+class signature_model:
+    ''' Define a signature model class that will be filled when trained '''
+
+    def __init__(self):
         pass
 
 
-class signature_classifier :
-    def __init__ (self, vcf, positive=None, negative=None,
-                        project_name='MSclassifier', reference_genome='GRCh38', exome=False, feature_list=['SBS96','ID83','DBS78'],
-                            model = signature_model()):
+class signature_classifier:
+    '''signature classifier is class defined to store entire projects,
+       train projects and share them.
+       '''
+
+    def __init__(self, vcf, positive=None, negative=None,
+                 project_name='MSclassifier', reference_genome='GRCh38',
+                 exome=False, feature_list=['SBS96', 'ID83', 'DBS78'],
+                 model=signature_model()):
         self.vcf = vcf
         self.positive = positive
-        self.negative= negative
+        self.negative = negative
         self.project_name = project_name
         self.reference_genome = reference_genome
         self.exome = exome
         self.feature_list = feature_list
-        self.model = model                  # model will be of signature_model class
+        self.model = model        # model will be of signature_model class
 
-    def load_vcf (self):
+    def load_vcf(self):
         load.load(self)
         self.sample_names = load.load_names(self)
-        self.data = load.dataset (self)
+        self.data = load.dataset(self)
 
-    def signature_train (self,ratio=0.7,end=3) :
-        self.model.signatures , self.model.features  = signature.train(self,ratio,end)
+    def signature_train(self, ratio=0.7, end=3):
+        self.model.signatures, self.model.features = signature.train(self, ratio, end)
 
-    def signature_fit (self):
+    def signature_fit(self):
         signature.fit(self)
 
-    def model_fit (self,model_type='neural'):
-        #self.model.model_type = model_type
-        model.fit(self,model_type)
+    def model_fit(self, model_type='neural'):
+        model.fit(self, model_type)
         self.model.importances = model.importances(self)
 
-    def test_check (self):
+    def test_check(self):
         model.test(self)
-
 
     def model_predict(self):
         model.predict(self)
-        values= self.data['training']
+        values = self.data['training']
         if not all(v == 0 for v in values):
-            self.confusion_matrix , self.accuracy= model.plot_confusion(self)
-            self.ROC_curve , self.AUC = model.ROC(self)
+            self.confusion_matrix, self.accuracy = model.plot_confusion(self)
+            self.ROC_curve, self.AUC = model.ROC(self)
         else:
-            self.accuracy=None
-        self.plot = model.plot_regression (self)
-
+            self.accuracy = None
+        self.plot = model.plot_regression(self)
 
     def export(self):
-        pickle.dump(self , open( self.vcf+'/output/' + self.project_name+ ".p", "wb" ) )
+        pickle.dump(self, open(self.vcf+'/output/'+self.project_name+".p", "wb"))
